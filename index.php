dd4bd484706f7bdca811b1580862d8f336eb899b
yyscolin <48078302+yyscolin@users.noreply.github.com>
2020-11-25 17:25:34 +0800

    Index - All stars are now shown instead of just 5

--- index.php
+++ index.php
@@ -11,11 +11,17 @@ include('public/html-mid.html');
 /** establish mysql connection */
 require('public/mysql_connections.php');
 
-/** Ger random stars */
+/** Get stars in random order */
 $randCount = 5;
-echo "<h2>Random Stars</h2>"
+echo "<h2>STARS</h2>"
     ."<div style='width:95vw;overflow-y:auto'><div style='text-align:center'>";
-$dbQuery = "select id, coalesce(concat(name_f, ' ', name_l), name_f) as name_e, name_j, dob, coalesce(count, 0) as count, display from stars join (select star, count(star) as count from casts group by star) as t on stars.id = t.star  where display = 1 union select 0, 'Others', null, null, (select count(id) from (select id from vids where id not in (select vid from casts) group by id) as t), null order by rand() limit 5";
+$dbQuery = "
+  select id, coalesce(concat(name_f, ' ', name_l), name_f) as name_e, name_j, dob, coalesce(count, 0) as count
+  from stars join (
+    select star, count(star) as count from casts group by star
+  ) as t on stars.id = t.star
+  where display = 1
+  order by rand()";
 $dbResponse = mysqli_query($con, $dbQuery);
 while ($r = mysqli_fetch_object($dbResponse)) {
     $img_src = "/media/stars/$r->id.jpg";
@@ -24,15 +30,28 @@ while ($r = mysqli_fetch_object($dbResponse)) {
     }
 
     echo "
-    <div class='star-box'>
-        <img onclick='window.location.href=\"/star/$r->id\"' src='$img_src'>
-        <p>$r->name_j</p>
-        <p>$r->name_e</p>
-        <p>$r->dob</p>
-        <a href='/star/$r->id'>$r->count Videos</a>
-    </div>";
+  <div class='star-box'>
+    <img onclick='window.location.href=\"/star/$r->id\"' src='$img_src'>
+    <p>$r->name_j</p>
+    <p>$r->name_e</p>
+    <p>$r->dob</p>
+    <a href='/star/$r->id'>$r->count Videos</a>
+  </div>";
 }
-echo "</div></div>";
+
+/** Print other stars box */
+$dbQuery = "select count(id) as count from vids where id not in (select vid from casts)";
+$dbResponse = mysqli_query($con, $dbQuery);
+$r = mysqli_fetch_object($dbResponse);
+echo "
+  <div class='star-box'>
+      <img onclick='window.location.href=\"/star/0\"' src='/media/stars/0.jpg'>
+      <p></p>
+      <p>Others</p>
+      <p></p>
+      <a href='/star/0'>$r->count Videos</a>
+  </div>";
+echo "\n</div></div>";
 
 /** Get random vids */
 $randCount = 5;
