381e0bf9e85ed979b2d024d458091f1f9b06ee5f
yyscolin <48078302+yyscolin@users.noreply.github.com>
2020-12-27 08:52:35 +0800

    Removed whitespace from #main-block in Stars page

--- pages/stars.php
+++ pages/stars.php
@@ -7,6 +7,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/public/common.php");
 print_page_header([  
   "<link rel='stylesheet' href='/styles/star-box.css'>",
   "<link rel='stylesheet' href='/styles/poster.css'>",
+  "<style>#main-block{font-size:0}</style>",
   "<title>".get_text("stars", ucfirst)." - Demo PHP Streamer</title>"
 ]);
 
