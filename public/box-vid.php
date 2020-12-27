de268395ed27a5722d1ca2542c8f5371318f6b31
yyscolin <48078302+yyscolin@users.noreply.github.com>
2020-12-27 09:55:57 +0800

    CSS - Vid poster link

--- public/box-vid.php
+++ public/box-vid.php
@@ -14,7 +14,7 @@ function print_vid_box($vid, $default_indentation=1) {
   }
 
   print_line("<div class='poster' style='$style'>", $default_indentation);
-  print_line("<h3>$title</h3>", $default_indentation + 1);
+  print_line("<p class='title'>$title</p>", $default_indentation + 1);
   print_line("<a href='$href'>", $default_indentation + 1);
   print_line("<img src='$img'>", $default_indentation + 2);
   print_line("</a>", $default_indentation + 1);
