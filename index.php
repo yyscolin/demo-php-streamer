f6687a85b75962777f8679ca58f7ba78e4786047
yyscolin <48078302+yyscolin@users.noreply.github.com>
2020-12-27 08:44:26 +0800

    Minor CSS fixes

--- index.php
+++ index.php
@@ -12,20 +12,23 @@ print_page_header([
 /** establish mysql connection */
 require_once($_SERVER['DOCUMENT_ROOT']."/public/mysql_connections.php");
 
-/** Get stars in random order */
-$randCount = 5;
-echo "<h2>STARS</h2>"
-    ."<div style='width:95vw;overflow-y:auto'><div style='text-align:center'>";
+print_line("<div id='main-block'>");
+
+/** Get random stars */
+$randCount = 50;
+print_line("<h2>Random Stars</h2>", 2);
+print_line("<div style='font-size:0;width:100%;overflow-y:auto'>", 2);
+print_line("<div style='text-align:center'>", 3);
 $dbQuery = "
   select id, name_l, name_f, name_j, dob, coalesce(count, 0) as count
   from stars left join (
     select star, count(star) as count from casts group by star
   ) as t on stars.id = t.star
   where display = 1
-  order by rand()";
+  order by rand() limit $randCount";
 $dbResponse = mysqli_query($con, $dbQuery);
 while ($r = mysqli_fetch_object($dbResponse)) {
-  print_star_box($r);
+  print_star_box($r, 4);
 }
 
 /** Print other stars box */
@@ -33,15 +36,16 @@ $dbQuery = "select count(id) as count from vids where id not in (select vid from
 $dbResponse = mysqli_query($con, $dbQuery);
 $r = mysqli_fetch_object($dbResponse);
 $r->name_f = "Others";
-print_star_box($r);
-echo "\n</div></div>";
+print_star_box($r, 4);
+
+print_line("</div>", 3);
+print_line("</div>", 2);
 
 /** Get random vids */
 $randCount = 5;
-echo "<h2>Random Videos</h2>";
+print_line("<h2>Random Videos</h2>", 2);
 $dbQuery = "select id, title from vids where status = 3 order by rand() limit $randCount";
 $dbResponse = mysqli_query($con, $dbQuery);
-print_line("<div id='main-block'>");
 while ($r = mysqli_fetch_object($dbResponse)) {
   print_vid_box($r, 2);
 }
