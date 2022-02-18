a41f50bc7ab280294019169559058268a29f57af
yyscolin <48078302+yyscolin@users.noreply.github.com>
2022-02-18 19:34:10 +1100

    Bugfix - Search page loads even if there is no query

--- search.php
+++ search.php
@@ -1,65 +1,65 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT']."/public/common.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/public/common.php");
 
-$type = $_GET['type'];
-$search_query = $_GET['query'];
+$type = isset($_GET["type"]) ? $_GET["type"] : null;
+$search_query = isset($_GET["query"]) ? $_GET["query"] : null;
 
-if (!isset($type) || !isset($search_query) || !in_array($type, ['movie', 'star'])) {
+if ($type && !in_array($type, ["movie", "star"])) {
   header("HTTP/1.0 400");
   exit();
 }
 
-$search_results = search_database_by_query($type, $search_query);
+function print_search_results($type, $search_query) {
+  if ($type && $search_query) {
+    $search_results = search_database_by_query($type, $search_query);
+    if (count($search_results) > 0) {?>
 
-function print_search_results() {
-  global $type;
-  global $search_results;
+    <table id="results-table" class="$type"><?php
 
-  if (count($search_results) > 0) {
-    echo "<table id='results-table' class='$type'>";
+      foreach ($search_results as $search_result) {
+        if ($type == "movie") {
+          $name = "<p>$search_result->name</p>";
+          $subtitle = $search_result->release_date ? "<p class=\"subtitle\">$search_result->release_date</p>" : "";
+          $media_path = $search_result->img;
+        } else {
+          $name = "<p>$search_result->name</p>";
+          $subtitle = join(", ", array_map(function($star_attribute) {
+            return "$star_attribute->key: $star_attribute->value";
+          }, $search_result->attributes));
+          $media_path = $search_result->img;
+        }?>
 
-    foreach ($search_results as $search_result) {
-      if ($type == 'movie') {
-        $name = "<p>$search_result->name</p>";
-        $subtitle = $search_result->release_date ? "<p class='subtitle'>$search_result->release_date</p>" : "";
-        $media_path = $search_result->img;
-      } else {
-        $name = "<p>$search_result->name</p>";
-        $subtitle = join(", ", array_map(function($star_attribute) {
-          return "$star_attribute->key: $star_attribute->value";
-        }, $search_result->attributes));
-        $media_path = $search_result->img;
-      }
-    
-      echo "<tr class='noselect' onclick='window.location.href=\"/$type/$search_result->id\"'>";
-      echo "<td><img src='$media_path'></td>";
-      echo "<td style='text-align:left'>$name$subtitle</td>";
-      echo "</tr>";
+      <tr class="noselect" onclick="window.location.href=`/<?=$type?>/<?=$search_result->id?>`">
+      <td><img src="<?=$media_path?>"></td>
+      <td style="text-align:left"><?=$name.$subtitle?></td>
+      </tr><?php
+
+      }?>
+
+    </table><?php
+
     }
-
-    echo "</table>";
   }
 }
 
 print_page_header([
-  "<link rel='stylesheet' href='/styles/page-search.css'>",
-  !$is_mobile && !$is_iPad ? "<link rel='stylesheet' href='/styles/page-search-web.css'>" : null,
-  "<title>".get_text("search", 'ucfirst')." - ".$_SERVER["PROJECT_TITLE"]."</title>"
+  "<link rel=\"stylesheet\" href=\"/styles/page-search.css\">",
+  !$is_mobile && !$is_iPad ? "<link rel=\"stylesheet\" href=\"/styles/page-search-web.css\">" : null,
+  "<title>".get_text("search", "ucfirst")." - ".$_SERVER["PROJECT_TITLE"]."</title>"
 ]);
 
 ?>
   
-  <div id='main-block'>
-    <form id='search-form'>
-      <select name='type'>
-        <option value='star'<?php if ($type == 'star') echo "selected='selected'"; ?>><?=get_text("stars", 'ucfirst')?></option>
-        <option value='movie'<?php if ($type == 'movie') echo "selected='selected'"; ?>><?=get_text("movies", 'ucfirst')?></option>
+  <div id="main-block">
+    <form id="search-form">
+      <select name="type">
+        <option value="star"<?php if ($type == "star") echo " selected=\"selected\""; ?>><?=get_text("stars", "ucfirst")?></option>
+        <option value="movie"<?php if ($type == "movie") echo " selected=\"selected\""; ?>><?=get_text("movies", "ucfirst")?></option>
       </select>
-      <input name='query' value='<?=$search_query?>'>
-      <button type='submit'><?=get_text("search", 'ucfirst')?></button>
-    </form>
-    <?php print_search_results(); ?>
+      <input name="query" value="<?=$search_query?>">
+      <button type="submit"><?=get_text("search", "ucfirst")?></button>
+    </form><?=print_search_results($type, $search_query)?>
 
   </div><?php
 
