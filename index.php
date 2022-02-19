e3fb3b17ebc610793a2270de4cad43ad184d5a46
yyscolin <48078302+yyscolin@users.noreply.github.com>
2022-02-19 20:13:13 +1100

    Random stars/ movies changed to featured stars/ movies of the day

--- index.php
+++ index.php
@@ -11,11 +11,16 @@ print_page_header([
 
 print_line("<div id='main-block'>");
 
-/** Get random stars */
-$random_count = 50;
-print_line("<h2>Random Stars</h2>", 2);
-print_line("<div style='font-size:0;width:100%;overflow-y:auto'>", 2);
-print_line("<div style='text-align:center'>", 3);
+$epoch_days = floor(time() / 24 / 60 / 60);
+
+/** Get featured stars */
+$min_stars_count = 5;
+$db_query = "SELECT FLOOR(COUNT(*)/$min_stars_count) AS count FROM stars WHERE status=1";
+$db_response = mysqli_query($mysql_connection, $db_query);
+$db_row = mysqli_fetch_object($db_response);
+$featured_group_count = $db_row->count;
+$todays_group_id = $epoch_days % $featured_group_count;
+
 $db_query = "
   SELECT id, name_$language AS name, COALESCE(count, 0) AS count
   FROM stars LEFT JOIN (
@@ -24,20 +29,44 @@ $db_query = "
       SELECT id FROM movies WHERE status=1
     ) GROUP BY star_id
   ) AS t ON stars.id=t.star_id
-  WHERE status=1
-  ORDER BY rand() LIMIT $random_count";
+  WHERE id IN (
+    SELECT id FROM (
+      SELECT ROW_NUMBER() OVER(ORDER BY id) % $featured_group_count AS group_id, id, name_en
+      FROM stars WHERE status=1
+    ) t1 WHERE group_id=$todays_group_id
+  )";
 $db_response = mysqli_query($mysql_connection, $db_query);
-while ($db_row = mysqli_fetch_object($db_response)) print_star_box($db_row, 4);
+if (mysqli_num_rows($db_response)) {
+  print_line("<h2>".get_text("stars of the day")."</h2>", 2);
+  print_line("<div style=\"font-size:0;width:100%;overflow-y:auto\">", 2);
+  print_line("<div style=\"text-align:center\">", 3);
+  while ($db_row = mysqli_fetch_object($db_response)) print_star_box($db_row, 4);
+  print_line("</div>", 3);
+  print_line("</div>", 2);
+}
 
-print_line("</div>", 3);
-print_line("</div>", 2);
+/** Get featured movies */
+$min_movies_count = 5;
+$db_query = "SELECT FLOOR(COUNT(*)/$min_movies_count) AS count FROM movies WHERE status=1";
+$db_response = mysqli_query($mysql_connection, $db_query);
+$db_row = mysqli_fetch_object($db_response);
+$featured_group_count = $db_row->count;
+$todays_group_id = $epoch_days % $featured_group_count;
 
-/** Get random movies */
-$random_count = 5;
-print_line("<h2>Random Videos</h2>", 2);
-$movies = get_movies_from_database();
-$random_indexes = array_rand($movies, $random_count);
-foreach ($random_indexes as $i) print_movie_box($movies[$i], 2);
+$db_query = "
+SELECT id, IFNULL(name_$language, '&ltNo Title&gt') AS name, release_date, duration
+FROM movies WHERE id IN (
+  SELECT id FROM (
+    SELECT ROW_NUMBER() OVER(ORDER BY id) % $featured_group_count AS group_id, id, name_en
+    FROM movies WHERE status=1
+  ) t1 WHERE group_id=$todays_group_id
+) ORDER BY db_timestamp DESC";
+$db_response = mysqli_query($mysql_connection, $db_query);
+if (mysqli_num_rows($db_response)) {
+  print_line("<h2>".get_text("movies of the day")."</h2>", 2);
+  while ($db_row = mysqli_fetch_object($db_response)) print_movie_box($db_row, 2);
+}
+
 print_line("</div>");
 
 print_page_footer();
--- public/languages.php
+++ public/languages.php
@@ -8,7 +8,15 @@ $dictionary = array(
     "release date"=>array("jp"=>"発売日"),
     "duration"=>array("jp"=>"収録時間"),
     "minutes"=>array("jp"=>"分鐘"),
-    "search"=>array("jp"=>"捜索")
+    "search"=>array("jp"=>"捜索"),
+    "stars of the day"=>array(
+        "en"=>"Featured Stars of Today",
+        "jp"=>"今日の注目のスター",
+    ),
+    "movies of the day"=>array(
+        "en"=>"Featured Movies of Today",
+        "jp"=>"今日の注目の映画",
+    ),
 );
 
 function get_text($text_reference, $option_callback=null) {
