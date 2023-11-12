71fc5be8307a3f66a893505cfdbebddfe37e80bc
yyscolin <48078302+yyscolin@users.noreply.github.com>
2023-11-12 13:57:44 +1100

    Bugfix - Stars/ Movies page not rendering properly if less than two pages of content

--- public/nav-pages.php
+++ public/nav-pages.php
@@ -5,27 +5,26 @@ function print_page_navbar($type, $count, $items_per_page) {
   global $is_mobile;
 
   $no_of_pages = ceil($count / $items_per_page);
+  if ($no_of_pages > 1) {
+    print_line("<div id=\"nav-pages\">");
+    for ($i = 1; $i <= $no_of_pages; $i++) {
+      if ($i == 1 || $i == 2 || $i == $no_of_pages) {
+        $classes = "nav-item-box";
+        if ($i == 2 && $no_of_pages != 2) $classes .= " middle";
+        print_line("<span class=\"$classes\">", 2);
+        print_line("<div style=\"margin:0;position:absolute;top:0\">", 3);
+      }
 
-  if ($no_of_pages < 2) return;
+      print_line("<span class=\"nav-item noselect\" data-page=\"$i\" onclick=\"window.location.hash=$i\">$i</span>", 4);
 
-  print_line("<div id=\"nav-pages\">");
-  for ($i = 1; $i <= $no_of_pages; $i++) {
-    if ($i == 1 || $i == 2 || $i == $no_of_pages) {
-      $classes = "nav-item-box";
-      if ($i == 2 && $no_of_pages != 2) $classes .= " middle";
-      print_line("<span class=\"$classes\">", 2);
-      print_line("<div style=\"margin:0;position:absolute;top:0\">", 3);
+      if ($i == 1 || $i == $no_of_pages - 1 || $i == $no_of_pages) {
+        print_line("</div>", 3);
+        print_line("</span>", 2);
+      }
     }
-
-    print_line("<span class=\"nav-item noselect\" data-page=\"$i\" onclick=\"window.location.hash=$i\">$i</span>", 4);
-
-    if ($i == 1 || $i == $no_of_pages - 1 || $i == $no_of_pages) {
-      print_line("</div>", 3);
-      print_line("</span>", 2);
-    }
+    print_line("</div>");
+    print_line("<div style=\"height:8vw\"></div>");
   }
-  print_line("</div>");
-  print_line("<div style=\"height:8vw\"></div>");
 
   echo "
   <script>
